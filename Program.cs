eae065314475111c9b8cbc0c0d46cd5a7d130f4a
Daria2945 <148208175+Daria2945@users.noreply.github.com>
2025-07-02 09:53:55 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Weapon
+{
+    private readonly int _bulletCountPerShoot = 1;
+    private int _bullets;
+
+    public bool CanShoot() =>
+        _bullets > 0 && _bullets >= _bulletCountPerShoot;
+
+    public void Shoot() =>
+        _bullets -= _bulletCountPerShoot;
+}
