9e15ebe3427c9229e903770803f4e492acf0076d
Daria2945 <148208175+Daria2945@users.noreply.github.com>
2025-07-02 21:41:36 +0300

    Update 2

--- Program.cs
+++ Program.cs
@@ -3,11 +3,23 @@
     private readonly int _bulletCountPerShoot = 1;
     private int _bullets;
 
-    private bool _canShoot => _bullets >= _bulletCountPerShoot;
+    public Weapon(int bullets, int bulletCountPerShoot)
+    {
+        if (bullets <= 0)
+            throw new ArgumentOutOfRangeException(nameof(bullets));
+
+        if (bulletCountPerShoot <= 0)
+            throw new ArgumentOutOfRangeException(nameof(bulletCountPerShoot));
+
+        _bullets = bullets;
+        _bulletCountPerShoot = bulletCountPerShoot;
+    }
+
+    private bool СanShoot => _bullets >= _bulletCountPerShoot;
 
     public void Shoot()
     {
-        if (_canShoot)
+        if (СanShoot)
             _bullets -= _bulletCountPerShoot;
     }
 }
